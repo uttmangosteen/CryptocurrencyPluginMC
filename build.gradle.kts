5377353a9d05a28ccf745c4f55727628c95d4614
uttmangosteen <noisimportantpoint@gmail.com>
2026-05-08 20:41:11 +0900

    環境構築

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    kotlin("jvm") version "2.4.0-Beta2"
+    id("com.gradleup.shadow") version "9.4.1"
+    id("xyz.jpenilla.run-paper") version "3.0.2"
+}
+
+repositories {
+    mavenCentral()
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.21.8-R0.1-SNAPSHOT")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("org.mongodb:mongodb-driver-kotlin-coroutine:5.6.5")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+
+    runServer {
+        // Configure the Minecraft version for our task.
+        // This is the only required configuration besides applying the plugin.
+        // Your plugin's jar (or shadowJar if present) will be used automatically.
+        minecraftVersion("1.21.8")
+        jvmArgs("-Xms2G", "-Xmx2G")
+    }
+
+    processResources {
+        val props = mapOf("version" to version)
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+}
---
+++ src/main/kotlin/io/github/uttmangosteen/cryptocurrencyPluginMC/Main.kt
@@ -0,0 +1,14 @@
+package io.github.uttmangosteen.cryptocurrencyPluginMC
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Main : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
